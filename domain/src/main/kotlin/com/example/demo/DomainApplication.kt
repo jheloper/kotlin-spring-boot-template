c03d1c44a48eb154ba0d600f952029d26709fd23
Joonghyeon Kim <jheloper9102@gmail.com>
2020-08-07 14:09:46 +0900

    h2db를 사용하여 domain 모듈 애플리케이션 실행하도록 수정

--- domain/src/main/kotlin/com/example/demo/DomainApplication.kt
+++ domain/src/main/kotlin/com/example/demo/DomainApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinSpringBootTemplateApplication
+class DomainApplication
 
 fun main(args: Array<String>) {
-	runApplication<KotlinSpringBootTemplateApplication>(*args)
+	runApplication<DomainApplication>(*args)
 }
