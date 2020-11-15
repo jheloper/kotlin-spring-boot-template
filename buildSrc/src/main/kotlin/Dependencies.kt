79dedabe8b7aa39df4156c93485c6e45cc6a4bb8
Joonghyeon Kim <jheloper9102@gmail.com>
2020-11-16 00:39:45 +0900

    buildSrc 라이브러리 의존성 추가

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,8 +1,13 @@
 object Versions {
     const val kotlin = "1.3.72"
-    const val spring_boot = "2.3.2.RELEASE"
+    const val spring_boot = "2.3.5.RELEASE"
+    const val spring_cloud = "2.2.4.RELEASE"
     const val aws_sdk = "2.13.23"
     const val testcontainers = "1.14.3"
+    const val slack = "1.1.3"
+    const val retrofit = "2.9.0"
+    const val sentry = "3.0.0"
+    const val jjwt = "0.11.2"
 }
 
 object Libraries {
@@ -16,6 +21,14 @@ object Libraries {
     const val spring_boot_test = "org.springframework.boot:spring-boot-starter-test:${Versions.spring_boot}"
     const val spring_boot_devtools = "org.springframework.boot:spring-boot-devtools:${Versions.spring_boot}"
     const val spring_boot_actuator = "org.springframework.boot:spring-boot-starter-actuator:${Versions.spring_boot}"
+    const val spring_boot_thymeleaf = "org.springframework.boot:spring-boot-starter-thymeleaf:${Versions.spring_boot}"
+    const val spring_boot_security = "org.springframework.boot:spring-boot-starter-security:${Versions.spring_boot}"
+    const val spring_boot_log4j2 = "org.springframework.boot:spring-boot-starter-log4j2:${Versions.spring_boot}"
+
+    // spring cloud
+    const val spring_cloud_netflix = "org.springframework.cloud:spring-cloud-starter-netflix:${Versions.spring_cloud}"
+    const val spring_cloud_netflix_zuul = "org.springframework.cloud:spring-cloud-starter-netflix-zuul:${Versions.spring_cloud}"
+    const val spring_cloud_oauth2 = "org.springframework.cloud:spring-cloud-starter-oauth2:${Versions.spring_cloud}"
 
     // aws sdk v1 (for testcontainers localstack)
     const val aws_sdk_v1_core = "com.amazonaws:aws-java-sdk-core:1.11.820"
@@ -43,8 +56,30 @@ object Libraries {
     // junit5
     const val junit_jupiter = "org.junit.jupiter:junit-jupiter:5.6.2"
 
+    // mockk
+    const val mockk = "io.mockk:mockk:1.10.0"
+
     // jaxb
     const val jaxb_api = "javax.xml.bind:jaxb-api:2.3.1"
+
+    // retrofit
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val retrofit_converter_jackson = "com.squareup.retrofit2:converter-jackson:${Versions.retrofit}"
+
+    // sentry
+    const val sentry = "io.sentry:sentry-spring-boot-starter:${Versions.sentry}"
+    const val sentry_log4j2 = "io.sentry:sentry-log4j2:${Versions.sentry}"
+
+    // slack
+    const val slack_api_client = "com.slack.api:slack-api-client:${Versions.slack}"
+    const val slack_api_client_kotlin_extension = "com.slack.api:slack-api-client-kotlin-extension:${Versions.slack}"
+    const val slack_api_model = "com.slack.api:slack-api-model:${Versions.slack}"
+    const val slack_api_model_kotlin_extension = "com.slack.api:slack-api-model-kotlin-extension:${Versions.slack}"
+
+    // jjwt
+    const val jjwt_api = "io.jsonwebtoken:jjwt-api:${Versions.jjwt}"
+    const val jjwt_impl = "io.jsonwebtoken:jjwt-impl:${Versions.jjwt}"
+    const val jjwt_jackson = "io.jsonwebtoken:jjwt-jackson:${Versions.jjwt}"
 
     // Testcontainers
     const val testcontainers = "org.testcontainers:testcontainers:${Versions.testcontainers}"
@@ -67,6 +102,6 @@ object Plugins {
     object Versions {
         const val kotlin = "1.3.72"
         const val dependency_management = "1.0.9.RELEASE"
-        const val spring_boot = "2.3.2.RELEASE"
+        const val spring_boot = "2.3.5.RELEASE"
     }
 }
